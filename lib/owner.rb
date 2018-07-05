5fb90cd7f00394010a30034ffa74b62d005afb37
Priscilla H <priscillamhuynh@gmail.com>
2018-07-05 13:38:26 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all=[]
+
+  def initialize(name)
+    @name=name
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all=[]
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def species
+    # self.species
+  end
 end
